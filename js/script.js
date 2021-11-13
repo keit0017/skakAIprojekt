48ffa2438b855b1443a39a20fb6d7748f167c7aa
B1gDaddyKane <jenschrj97@gmail.com>
2021-11-13 16:48:37 +0100

    Made it so only legal moves are allowed

--- js/script.js
+++ js/script.js
@@ -5,11 +5,39 @@
  * Copyright (c) 2021 Jens Jensen & Keith Birongo
  */
 
-var game = new Chess();
+let game = new Chess();
+//We need to initialize board otherwise we get error when called in precompile.
+let board = null;
+function onDragStart(source, piece, position, orientation) {
 
-var config = {
+    if (game.game_over()) return false;
+    // It checks whos turn it is and if the piece we try to move is the opposite color.
+    if ((game.turn() === "w" && piece.search("/^b/") !== -1) || 
+        (game.turn() === "b" && piece.search("/^w/") !== -1)) {
+            return false;
+        }
+}
+
+function onDrop(source, target) {
+    let move = game.move({
+        from: source,
+        to: target,
+        promotion: "q",
+    });
+
+    if (move === null) return false;
+}
+
+function onSnapEnd() {
+    board.position(game.fen())
+}
+
+let config = {
   draggable: true,
   position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd,
 };
 
-var board = new Chessboard('board', config);
+board = new Chessboard('board', config);
